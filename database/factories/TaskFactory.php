ad2b4bd5f1a44c9a6fd065e88a18401047ad91a1
rohitvispute2151 <rohitvispute2151@gmail.com>
2024-02-06 16:24:25 +0530

    first commit

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    public function definition(): array {
+        return [
+            'user_id' => User::factory(),
+            'title' => fake()->catchPhrase(),
+            'description' => fake()->realText($maxNbChars = 200, $indexSize = 2),
+            'due-date' => fake()->dateTime(),
+
+        ];
+    }
+
+}
